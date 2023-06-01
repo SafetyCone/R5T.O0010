d6ef42e23830503e98eda6d4394107a930181025
davidcoats <david@getsafetycone.com>
2023-06-01 11:05:49 -0700

    Initial commit.

---
+++ source/R5T.O0010/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.O0010
+{
+	/// <summary>
+	/// Project generation operations umbrella library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.O0010/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.O0010
+Project generation operations umbrella library.
