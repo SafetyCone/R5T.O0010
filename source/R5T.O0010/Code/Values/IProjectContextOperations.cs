6d21b0a5fefc3d0f5b2872cf34590a3279e99d9f
davidcoats <david@getsafetycone.com>
2023-06-01 18:07:03 -0700

    Interim changes.

---
+++ source/R5T.O0010/Code/Values/IProjectContextOperations.cs
@@ -0,0 +1,18 @@
+using System;
+
+using R5T.T0131;
+
+
+namespace R5T.O0010
+{
+    /// <summary>
+    /// Generation project context operations.
+    /// </summary>
+    [ValuesMarker]
+    public partial interface IProjectContextOperations : IValuesMarker,
+        O0011.IProjectContextOperations,
+        O0012.IProjectContextOperations
+    {
+
+    }
+}
